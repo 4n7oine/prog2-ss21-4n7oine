11a5d09a5cd8ccd6ffb9c7ecc5c86025521b921e
4n7oine <a.lanners@outlook.com>
2022-03-18 10:08:54 +0100

    Folders, Package & Result-Class created

---
+++ src/main/java/prg2project/Result.java
@@ -0,0 +1,12 @@
+package prg2project;
+import java.util.List;
+
+public class Result {
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
