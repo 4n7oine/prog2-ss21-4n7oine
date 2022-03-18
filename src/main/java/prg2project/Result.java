fca98b85620d79bd861c30eca7f880578d4ea691
4n7oine <a.lanners@outlook.com>
2022-03-18 10:47:06 +0100

    Mainmethod of Result Class => Input

--- src/main/java/prg2project/Result.java
+++ src/main/java/prg2project/Result.java
@@ -1,5 +1,7 @@
 package prg2project;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Result {
     public static List<Integer> gradingStudents(List<Integer> grades) {
@@ -7,6 +9,14 @@ public class Result {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter Number of Students");
+        int students = scan.nextInt();
+        System.out.println(students);
+        List<Integer> grades = new ArrayList<>();
+        for (int i=0; i<students;i++){
+            System.out.println("Enter Grade of Student" + (i+1));
+            grades.add(scan.nextInt());
+        }
     }
 }
