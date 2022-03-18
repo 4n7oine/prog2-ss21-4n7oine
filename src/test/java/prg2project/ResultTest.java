748a9718346e0d2d1e312fd515a137a546f4d420
4n7oine <a.lanners@outlook.com>
2022-03-18 10:12:36 +0100

    First Testmethode

---
+++ src/test/java/prg2project/ResultTest.java
@@ -0,0 +1,14 @@
+package prg2project;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ResultTest {
+
+    @Test
+    void gradingStudents() {
+    }
+}
