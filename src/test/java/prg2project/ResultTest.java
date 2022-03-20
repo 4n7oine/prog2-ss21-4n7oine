86c219c872977dbfd177cf28819b74122d32d771
4n7oine <a.lanners@outlook.com>
2022-03-20 20:42:23 +0100

    First Testcase

--- src/test/java/prg2project/ResultTest.java
+++ src/test/java/prg2project/ResultTest.java
@@ -2,15 +2,28 @@ package prg2project;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ResultTest {
 
     @Test
-    void gradingStudents() {
-
-
+    @DisplayName("Grades below 38")
+    void gradingStudentsbelow38() {
+        List<Integer> input = new ArrayList<>();
+        List<Integer> expected = new ArrayList<>();;
+        for(int i=0; i<38; i++){
+            input.add(i);
+            expected.add(i);
+        }
+        List<Integer> actual = Result.gradingStudents(input);
+        assertEquals(expected, actual);
     }
+
+
 }
